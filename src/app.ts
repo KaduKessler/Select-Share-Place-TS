d4baa6ac0523af8e94617ea5e7b5000791e17282
KaduKessler <kadukessler@gmail.com>
2024-05-11 16:33:43 -0300

    ✨ feat: Implement Google Maps search functionality with map display
    - Refactored code to use Google Maps API key from .env
    - Added error handling to check for unsuccessful API responses
    - **Enhanced functionality: Display address location on map upon search**
        - Created a Google Maps instance and added a marker to the map
        - Centered the map on the retrieved coordinates
    
    This commit enables secure management of the Google Maps API key, improves error handling, and adds the functionality to display the searched address location on a map.

--- src/app.ts
+++ src/app.ts
@@ -3,25 +3,26 @@ import axios from "axios";
 const form = document.querySelector("form")! as HTMLFormElement;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
-const GOOGLE_API_KEY = "API_KEY";
-
-type GoogleGeocodingResponse = {
-  results: { geometry: { location: { lat: number; long: number } } }[];
-  status: "OK" | "ZERO_RESULTS";
-};
-
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
   axios
-    .get<GoogleGeocodingResponse>(
+    .get(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
-      )}&key=${GOOGLE_API_KEY}`
+      )}&key=${process.env.GOOGLE_API_KEY}`
     )
     .then((response) => {
-      console.log(response);
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map")!, {
+        center: coordinates,
+        zoom: 16,
+      });
+      new google.maps.Marker({ position: coordinates, map: map });
     })
     .catch((error) => {
       console.log(error);
@@ -29,3 +30,9 @@ function searchAddressHandler(event: Event) {
 }
 
 form.addEventListener("submit", searchAddressHandler);
+
+var script = document.createElement("script");
+script.src = `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&loading=async`;
+script.async = true;
+
+document.head.appendChild(script);
